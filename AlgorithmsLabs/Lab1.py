f8455c9194c9a820c0f7726fb61724f84cde1b0e
Krut1la <aleksey.krutko@gmail.com>
2021-02-01 15:19:42 +0200

    Lab 2. Finalizing

--- AlgorithmsLabs/Lab1.py
+++ AlgorithmsLabs/Lab1.py
@@ -5,43 +5,30 @@ Desc:   Algorithms Lab 1. Var 10. 2021
 """
 
 import math
-from Utils import get_input_source, get_option
+
+from Input import get_input_source, get_option, InputError
 
 
-def linear_algo(input_source):
-    print("Lab 1. Linear algorithm. Variant 10.")
-    a = input_source("a", float)
-    b = input_source("b", float)
-    c = input_source("c", float)
-    d = input_source("d", float)
-
+def linear_algo(a, b, c, d):
     try:
         Y1 = ((math.sqrt(a) + b ** 2) / (math.sqrt(b) - a ** 2)) + (math.sqrt((a * b) / (c * d)))
-        print("Y1 = {}".format(Y1))
     except ZeroDivisionError:
         print("Calculation failed!")
+        Y1 = float('nan')
+
+    return Y1
 
 
-def condition_algo(input_source):
-    print("Lab 1. Condition algorithm. Variant 10.")
-    a = input_source("a", float)
-    c = input_source("c", float)
-    k = input_source("k", float)
-
+def condition_algo(a, c, k):
     if k < 10:
         y = (a + c) ** 4 + (a - c) ** 2
     else:
         y = (a - c) ** 3 + (a + c) ** 2
 
-    print("y = {}".format(y))
+    return y
 
 
-def cyclic_algo(input_source):
-    print("Lab 1. Cyclic algorithm. Variant 10.")
-    a = input_source("a", float)
-    b = input_source("b", float)
-    p = input_source("p", int)
-
+def cyclic_algo(a, b, p):
     f = 0
 
     for i in range(1, p + 1):
@@ -49,20 +36,46 @@ def cyclic_algo(input_source):
             for k in range(1, p + 1):
                 f = f + i * i * j * i * j * k * math.sqrt(a + b)
 
-    print("f = {}".format(f))
+    return f
 
 
 def main():
-    input_source = get_input_source()
-
     option = get_option("Select algorithm (1 - Linear, 2 - Condition, 3 - Cyclic):", 3)
 
     if option == 1:
-        linear_algo(input_source)
+        input_source = get_input_source("lab1_linear_algo")
+        print("Lab 1. Linear algorithm. Variant 10.")
+        try:
+            a = input_source.read_var("a", float, float("-inf"), float("inf"))
+            b = input_source.read_var("b", float, float("-inf"), float("inf"))
+            c = input_source.read_var("c", float, float("-inf"), float("inf"))
+            d = input_source.read_var("d", float, float("-inf"), float("inf"))
+        except InputError as ir:
+            print("Error wile reading data from file: ", ir)
+        else:
+            print("Y1 = {:.2f}".format(linear_algo(a, b, c, d)))
     elif option == 2:
-        condition_algo(input_source)
+        input_source = get_input_source("lab1_condition_algo")
+        print("Lab 1. Condition algorithm. Variant 10.")
+        try:
+            a = input_source.read_var("a", float, float("-inf"), float("inf"))
+            c = input_source.read_var("c", float, float("-inf"), float("inf"))
+            k = input_source.read_var("k", float, float("-inf"), float("inf"))
+        except InputError as ir:
+            print("Error wile reading data from file: ", ir)
+        else:
+            print("y = {:.2f}".format(condition_algo(a, c, k)))
     elif option == 3:
-        cyclic_algo(input_source)
+        input_source = get_input_source("lab1_cyclic_algo")
+        print("Lab 1. Cyclic algorithm. Variant 10.")
+        try:
+            a = input_source.read_var("a", float, float("-inf"), float("inf"))
+            b = input_source.read_var("b", float, float("-inf"), float("inf"))
+            p = input_source.read_var("p", int, 1, 10000)
+        except InputError as ir:
+            print("Error wile reading data from file: ", ir)
+        else:
+            print("f = {:.2f}".format(cyclic_algo(a, b, p)))
 
 
 main()
--- AlgorithmsLabs/Lab2.py
+++ AlgorithmsLabs/Lab2.py
@@ -4,24 +4,18 @@ Auth:   Oleksii Krutko, IO-z91
 Desc:   Algorithms Lab 2. Var 10. 2021
 """
 
-import math
-from Utils import input_from_file, get_option
+import random
+
+import matplotlib.pyplot as plt
+import time
+from Input import get_input_source, InputError, get_option
 
 
-def fast_bubble_sort(input_source):
-    print("Lab 2. Fast bubble sort. Variant 10.")
-
-    n = input_source("n", int)
-
-    a = [n]
-    for i in range(0, n):
-        a.append(input_source("a[{}]".format(i + 1), float))
-
-    print("Sorting...")
-
+def fast_bubble_sort(a):
+    n = len(a)
     k = 1
 
-    while k < n:
+    while True:
         P = 0
         for i in range(0, n - k):
             if a[i] > a[i + 1]:
@@ -35,15 +29,113 @@ def fast_bubble_sort(input_source):
 
         k = k + 1
 
-    for i in range(0, n):
-        print("a[{}] = {}".format(i + 1, a[i]))
+
+def analyze(a, num_iterations):
+    """
+    Runs sorting num_iterations times and calculates average time spent
+    :param a: array with data
+    :param num_iterations:
+    :return: average time in ms
+    """
+
+    diff_ns = 0.0
+    for i in range(0, num_iterations):
+        a_copy = a.copy()
+        start_time = time.process_time()
+
+        fast_bubble_sort(a_copy)
+
+        end_time = time.process_time()
+
+        diff_ns = diff_ns + (end_time - start_time)
+
+    diff_arg_ms = 1000 * diff_ns/num_iterations
+
+    return diff_arg_ms
 
 
 def main():
-    # input_source = get_input_source()
-    input_source = input_from_file
+    option = get_option("Select job (1 - demo, 2 - speed analyze):", 2)
 
-    fast_bubble_sort(input_source)
+    if option == 1:
+        input_source = get_input_source("lab2_fast_bubble")
+        print("Lab 2. Fast bubble sort demo. Variant 10.")
+        try:
+            n = input_source.read_var("n", int, 1, 10000)
+
+            a = []
+            for i in range(0, n):
+                a.append(input_source.read_var("a[{}]".format(i + 1), float, float("-inf"), float("inf")))
+
+        except InputError as ir:
+            print("Error wile reading data from file: ", ir)
+        else:
+            print("Sorting...")
+
+            fast_bubble_sort(a)
+
+            for i in range(0, n):
+                print("a[{}] = {}".format(i + 1, a[i]))
+    elif option == 2:
+        print("Lab 2. Fast bubble sort speed analyze. Variant 10.")
+        fig, axs = plt.subplots()
+        fig.canvas.set_window_title('Lab 2. Fast bubble sort speed analyze. Variant 10.')
+        axs.set_title('g(n)')
+        axs.set_ylabel('time (ms)')
+        axs.set_xlabel('n (elements)')
+
+        print("Worst case. Elements presorted wrong way")
+        n_step = 10
+        x = []
+        y = []
+        for n in range(n_step, n_step*10, n_step):
+            a = []
+
+            a_value = -n/2
+            for i in range(0, n):
+                a.append(-(a_value + i))
+
+            time_ms = analyze(a, 1)
+
+            x.append(n)
+            y.append(time_ms)
+
+            axs.plot(x, y, color='red')
+
+        print("Best case. Elements presorted right way")
+
+        x = []
+        y = []
+        for n in range(n_step, n_step*10, n_step):
+            a = []
+            a_value = -n / 2
+            for i in range(0, n):
+                a.append(a_value + i)
+
+            time_ms = analyze(a, 1)
+
+            x.append(n)
+            y.append(time_ms)
+
+            axs.plot(x, y, color='green')
+
+        print("Average case. Elements not sorted")
+
+        x = []
+        y = []
+        for n in range(n_step, n_step*10, n_step):
+            a = []
+            for i in range(0, n):
+                a.append((random.random() - 0.5) * n / 2)
+
+            time_ms = analyze(a, 1)
+
+            x.append(n)
+            y.append(time_ms)
+
+            axs.plot(x, y, color='brown')
+
+        plt.show()
 
 
 main()
